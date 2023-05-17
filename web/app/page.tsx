ac4ea9bcbc8c1dc4afd632abb71dd3b8060f5237
Joelson Junior <joelson.ejr@gmail.com>
2023-05-17 15:44:32 -0300

    started client config

---
+++ web/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/Button";
+
+export default function Home() {
+  return (
+    <h1>
+      Hello World
+      <Button title="Jon"/>
+      <Button title="Sabrina"/>
+     </h1>
+  )
+}

2131177705a04d3c81a8c84f9a4f8e47770c3e80
Joelson Junior <joelson.ejr@gmail.com>
2023-06-05 11:18:35 -0300

    study session June 06

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,11 +1,3 @@
-import { Button } from "@/components/Button";
-
 export default function Home() {
-  return (
-    <h1>
-      Hello World
-      <Button title="Jon"/>
-      <Button title="Sabrina"/>
-     </h1>
-  )
+  return <div className="rounded text-2xl font-bold">oi</div>
 }
